f34dfd6d3998b30b6b70b5eed125b82fa5683cb7
main <alphapro100@gmail.com>
2023-05-03 18:05:25 +0300

    Отработка ошибок в модели

--- police/models.py
+++ police/models.py
@@ -50,73 +50,145 @@ class EncouragementChoice(models.TextChoices):
 
 
 class Encouragement(models.Model):
-    kind_of_encouragement = models.CharField(choices=EncouragementChoice, blank=True,)
+    completion_of_the_disciplinary_contract = "Дострокове зняття дисциплінарного стягнення"
+    listed_on_the_board = "Занесення на дошку пошани"
+    reword_money = "Заохочення грошовою винагородою"
+    reword_gift = "Заохочення цінним подарунком"
+    vacation = "Надання додаткової плачуваної відпустки тривалістю до п\'яти діб"
+    departmental_awards_of_the_National_Police_of_Ukraine = "Заохочення відомчими заохочувальними відзнаками Національної поліції України"
+    departmental_awards_of_the_Ministry_of_Internal_Affairs_of_Ukraine = "Заохочення відомчими заохочувальними відзнаками Міністерства внутрішніх справ України"
+    early_assignment_of_rank = "Дострокове присвоєння чергового спеціального звання"
+    assignment_of_the_title_higher_by_one_degree = "Присвоєння спеціального звання, вищого на одну ступінь від звання, передбаченого займаною штатною посадою"
+    firearm = "Заохочення відомчою заохочувальною відзнакою Міністерства внутрішніх справа України \'Вогнепальна зброя'"
+    cold_weapon = "Заохочення відомчою заохочувальною відзнакою Міністерства внутрішніх справа України \'Холодна зброя'"
+    EncouragementChoice = [
+        (completion_of_the_disciplinary_contract, "Дострокове зняття дисциплінарного стягнення"),
+        (listed_on_the_board, "Занесення на дошку пошани"),
+        (reword_money, "Заохочення грошовою винагородою"),
+        (reword_gift, "Заохочення цінним подарунком"),
+        (vacation, "Надання додаткової плачуваної відпустки тривалістю до п\'яти діб"),
+        (departmental_awards_of_the_National_Police_of_Ukraine,
+         "Заохочення відомчими заохочувальними відзнаками Національної поліції України"),
+        (departmental_awards_of_the_Ministry_of_Internal_Affairs_of_Ukraine,
+         "Заохочення відомчими заохочувальними відзнаками Міністерства внутрішніх справ України"),
+        (early_assignment_of_rank, "Дострокове присвоєння чергового спеціального звання"),
+        (assignment_of_the_title_higher_by_one_degree,
+         "Присвоєння спеціального звання, вищого на одну ступінь від звання, передбаченого займаною штатною посадою"),
+        (firearm,
+         "Заохочення відомчою заохочувальною відзнакою Міністерства внутрішніх справа України \'Вогнепальна зброя'"),
+        (cold_weapon,
+         "Заохочення відомчою заохочувальною відзнакою Міністерства внутрішніх справа України \'Холодна зброя'"),
+    ]
+    kind_of_encouragement = models.CharField(choices=EncouragementChoice, blank=True, max_length=110)
     order = models.CharField(max_length=100, blank=True)
     date_of_the_order = models.DateField(auto_now=False, auto_now_add=False)
-    Issuer_of_the_order = models.CharField(choices=IssuerChoice.choices, blank=True, )
-
-
-class TypeOfRecoveryChoice(models.TextChoices):
-    remark = '0', 'Зауваження'
-    reprimand = '1', 'Догана'
-    severe_reprimand = '2', "Сувора догана"
-    warning = '3', "Попередження про неповну посадову відповідність "
-    demotion = '4', "Пониження у спеціальному званні на одну ступінь"
-    dismissal_from_office = '5', "Звільнення з посада"
-    dismissal_from_the_police = '6', "Звільнення зі служби в поліції"
+    NPD = 'НПУ'
+    DPS = 'ДПО'
+    UPO = 'УПО'
+    IssuerChoice = [
+        (NPD, 'НПУ'),
+        (DPS, 'ДПО'),
+        (UPO, 'УПО]'),
+        ]
+    Issuer_of_the_order = models.CharField(choices=IssuerChoice, blank=True, max_length=3)
 
 
 class TypeOfRecovery(models.Model):
-    type_of_recovery = models.CharField(choices=TypeOfRecoveryChoice, blank=True,)
+    remark = 'Зауваження'
+    reprimand = 'Догана'
+    severe_reprimand = "Сувора догана"
+    warning = "Попередження про неповну посадову відповідність"
+    demotion = "Пониження у спеціальному званні на одну ступінь"
+    dismissal_from_office = "Звільнення з посада"
+    dismissal_from_the_police = "Звільнення зі служби в поліції"
+    TypeOfRecoveryChoice = [
+        (remark, 'Зауваження'),
+        (reprimand, 'Догана'),
+        (severe_reprimand, "Сувора догана"),
+        (warning, "Попередження про неповну посадову відповідність"),
+        (demotion, "Пониження у спеціальному званні на одну ступінь"),
+        (dismissal_from_office, "Звільнення з посада"),
+        (dismissal_from_the_police, "Звільнення зі служби в поліції"),
+    ]
+    type_of_recovery = models.CharField(choices=TypeOfRecoveryChoice, blank=True, max_length=50)
     order = models.CharField(max_length=100, blank=True)
     date_of_the_order = models.DateField(auto_now=False, auto_now_add=False)
-    Issuer_of_the_order = models.CharField(choices=IssuerChoice.choices, blank=True, )
-
-
-class PersonnelCategory(models.TextChoices):
-    junior_senior_staff = '0', "МНС"
-    senior_staff = '1', "НС"
+    NPD = 'НПУ'
+    DPS = 'ДПО'
+    UPO = 'УПО'
+    IssuerChoice = [
+        (NPD, 'НПУ'),
+        (DPS, 'ДПО'),
+        (UPO, 'УПО]'),
+        ]
+    Issuer_of_the_order = models.CharField(choices=IssuerChoice, blank=True, max_length=3)
 
 
 class Vocation(models.Model):
     order = models.CharField(max_length=10, blank=True)
     date_of_the_order = models.DateField(auto_now=False, auto_now_add=False)
-    number_of_days = models.IntegerField(max_length=2, blank=True)
+    number_of_days = models.IntegerField(blank=True)
     vacation_start_date = models.DateField(auto_now=False, auto_now_add=False)
     vacation_end_date = models.DateField(auto_now=False, auto_now_add=False)
 
 
-class PositionCategory(models.TextChoices):
-    police_response_platoon = '0', "поліцейський взводу реагування"
-    a_police_officer_of_the_guard_platoon = '1', "поліцейський взводу ОО та ПБ"
-    police_driver = '2', "поліцейський - водій ВР"
-    junior_inspector_of_the_security_platoon = '3', "молодший інспектор взводу ОО та ПБ"
-    response_platoon_junior_inspector = '4', "молодший інспектор ВР"
-    security_platoon_inspector = '5', "інспектор взводу ОО та ПБ"
-    deputy_platoon_commander = '6', "заступник командира взводу"
-    platoon_leader = '7', "командир взводу"
-    company_commander = '8', "командир роти"
-    deputy_company_commander = '9', "заступник командира роти"
-    battalion_commander = '10', "командир батальйону"
-    deputy_battalion_commander = '11', "заступник командира батальйону"
-
-
 class Position(models.Model):
-    available_position = models.CharField(choices=PositionCategory, default=PositionCategory.a_police_officer_of_the_guard_platoon,)
-    position_category = models.CharField(choices=PersonnelCategory, default=PersonnelCategory.junior_senior_staff,)
+    police_response_platoon = "поліцейський взводу реагування"
+    a_police_officer_of_the_guard_platoon = "поліцейський взводу ОО та ПБ"
+    police_driver = "поліцейський - водій ВР"
+    junior_inspector_of_the_security_platoon = "молодший інспектор взводу ОО та ПБ"
+    response_platoon_junior_inspector = "молодший інспектор ВР"
+    security_platoon_inspector = "інспектор взводу ОО та ПБ"
+    deputy_platoon_commander = "заступник командира взводу"
+    platoon_leader = "командир взводу"
+    company_commander = "командир роти"
+    deputy_company_commander = "заступник командира роти"
+    battalion_commander = "командир батальйону"
+    deputy_battalion_commander = "заступник командира батальйону"
+    PositionCategory = [
+        (police_response_platoon, "поліцейський взводу реагування"),
+        (a_police_officer_of_the_guard_platoon, "поліцейський взводу ОО та ПБ"),
+        (police_driver, "поліцейський - водій ВР"),
+        (junior_inspector_of_the_security_platoon, "молодший інспектор взводу ОО та ПБ"),
+        (response_platoon_junior_inspector, "молодший інспектор ВР"),
+        (security_platoon_inspector, "інспектор взводу ОО та ПБ"),
+        (deputy_platoon_commander, "заступник командира взводу"),
+        (platoon_leader, "командир взводу"),
+        (company_commander, "командир роти"),
+        (deputy_company_commander, "заступник командира роти"),
+        (battalion_commander, "командир батальйону"),
+        (deputy_battalion_commander, "заступник командира батальйону"),
+    ]
+    available_position = models.CharField(choices=PositionCategory, default=a_police_officer_of_the_guard_platoon, max_length=40)
+    junior_senior_staff = "МНС"
+    senior_staff = "НС"
+    PersonnelCategory = [
+        (junior_senior_staff, "МНС"),
+        (senior_staff, "НС"),
+    ]
+    position_category = models.CharField(choices=PersonnelCategory, default=junior_senior_staff,
+                                         max_length=20)
     appointment_date = models.DateField(auto_now=False, auto_now_add=False)
     appointment_order = models.CharField(max_length=10, blank=True)
-    Issuer_of_the_order = models.CharField(choices=IssuerChoice, default=IssuerChoice.UPO,)
+    NPD = 'НПУ'
+    DPS = 'ДПО'
+    UPO = 'УПО'
+    IssuerChoice = [
+        (NPD, 'НПУ'),
+        (DPS, 'ДПО'),
+        (UPO, 'УПО]'),
+    ]
+    Issuer_of_the_order = models.CharField(choices=IssuerChoice, max_length=3)
 
 
 class Rank(models.Model):
-    rank = models.CharField(choices=RankChoice.choices, default=RankChoice.soldier, )
+    rank = models.CharField(choices=RankChoice.choices, default=RankChoice.soldier, max_length=27)
     date_of_the_order = models.DateField(auto_now=False, auto_now_add=False)
     order = models.CharField(max_length=100, blank=True)
-    Issuer_of_the_order = models.CharField(choices=IssuerChoice.choices, default=IssuerChoice.UPO,)
+    Issuer_of_the_order = models.CharField(choices=IssuerChoice.choices, default=IssuerChoice.UPO, max_length=5)
 
 
-class Family (models.Model):
+class Family(models.Model):
     mother = models.CharField(max_length=100, blank=True)
     father = models.CharField(max_length=100, blank=True)
     wife = models.CharField(max_length=100, blank=True)
@@ -152,7 +224,7 @@ class Family (models.Model):
     place_of_actual_residence_wife = models.CharField(max_length=150, blank=True)
     place_of_actual_residence_son = models.CharField(max_length=150, blank=True)
     place_of_actual_residence_daughter = models.CharField(max_length=150, blank=True)
-    family_camp = models.CharField(choices=FamilyChoice.choices, default=FamilyChoice.unmarried, )
+    family_camp = models.CharField(choices=FamilyChoice.choices, default=FamilyChoice.unmarried, max_length=15)
 
 
 class Education(models.Model):
@@ -163,7 +235,7 @@ class Education(models.Model):
     document_about_education = models.CharField(max_length=100)
     specialty = models.CharField(max_length=50)
     profession = models.CharField(max_length=50)
-    form_of_education = models.CharField(20)
+    form_of_education = models.CharField(max_length=20)
 
 
 class EducationPolice(models.Model):
@@ -177,14 +249,14 @@ class EducationPolice(models.Model):
     date_of_graduation_from_the_retraining = models.DateField(auto_now=False, auto_now_add=False)
 
 
-class FramesPolice (models.Model):
-    number_private_business = models.IntegerField(max_length=6)
-    subdivision = models.CharField(choices=SubdivisionChoice.choices, default=SubdivisionChoice.UGSO,)
+class FramesPolice(models.Model):
+    number_private_business = models.IntegerField(blank=True)
+    subdivision = models.CharField(choices=SubdivisionChoice.choices, default=SubdivisionChoice.UGSO, max_length=5)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     Surname = models.CharField(max_length=20)
     First_name = models.CharField(max_length=20)
     Last_name = models.CharField(max_length=20)
-    special_number = models.IntegerField(max_length=9, unique=True, blank=False)
+    special_number = models.IntegerField(unique=True, blank=True)
     sex = models.CharField(max_length=9)
     date_of_recruitment = models.DateField(auto_now=False, auto_now_add=False)
     date_of_call_of_the_service = models.DateField(auto_now=False, auto_now_add=False)
